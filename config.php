7c904941e7b6e1539c177c13e3a94fcc1182aac5
fragnaud <dylanfragnaud16@gmail.com>
2025-01-22 18:33:20 +0100

    Correction de la co

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $url_parts = parse_url($db_url);
 $db_host = $url_parts['q2gen47hi68k1yrb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'];
 $db_user = $url_parts['r92r3g8xxrrs5dfd'];
 $db_pass = $url_parts['fkadbr0r78s8j5iv'];
-$db_name = ltrim($url_parts['path'], '/');
+$db_name = ltrim($url_parts['e3pqkuprmh1aj5nm'], '/');
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
