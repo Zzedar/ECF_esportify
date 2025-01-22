624792c049fd71c8072c082fc2697801db2eda42
fragnaud <dylanfragnaud16@gmail.com>
2025-01-22 18:31:06 +0100

    Correction de la co

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $db_url = getenv('JAWSDB_URL');
 $url_parts = parse_url($db_url);
 
-$db_host = $url_parts['e3pqkuprmh1aj5nm'];
+$db_host = $url_parts['q2gen47hi68k1yrb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'];
 $db_user = $url_parts['r92r3g8xxrrs5dfd'];
 $db_pass = $url_parts['fkadbr0r78s8j5iv'];
 $db_name = ltrim($url_parts['path'], '/');
