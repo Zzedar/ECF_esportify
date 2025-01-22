a58eca935ab97a87cd2bc7f80d28397043e5720e
fragnaud <dylanfragnaud16@gmail.com>
2025-01-22 18:54:08 +0100

    correction

--- config.php
+++ config.php
@@ -1,15 +1,17 @@
 <?php
-$db_url = getenv('JAWSDB_URL');
-$url_parts = parse_url($db_url);
+$url = parse_url(getenv("JAWSDB_URL")); // Récupérer les infos depuis la config Heroku
 
-$db_host = 'q2gen47hi68k1yrb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-$db_user = 'r92r3g8xxrrs5dfd';
-$db_pass = 'fkadbr0r78s8j5iv';
-$db_name = 'e3pqkuprmh1aj5nm';
+$host = $url["q2gen47hi68k1yrb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"];
+$username = $url["r92r3g8xxrrs5dfd"];
+$password = $url["fkadbr0r78s8j5iv"];
+$database = substr($url["e3pqkuprmh1aj5nm"], 1);
 
-try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+// Connexion à la base MySQL
+$conn = new mysqli($host, $username, $password, $database);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
 }
+echo "Connexion réussie !";
 ?>
